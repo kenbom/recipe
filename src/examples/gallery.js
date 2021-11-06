4ad4e9fd6cc6a41439513c36dc8c1a796438a240
kenbom <tsutomu.kimura@nifty.ne.jp>
2021-11-06 21:12:03 +0000

    add image handling

---
+++ src/examples/gallery.js
@@ -0,0 +1,56 @@
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import { GatsbyImage } from "gatsby-plugin-image"
+import styled from "styled-components"
+const query = graphql`
+  {
+    allFile(filter: { extension: { ne: "svg" } }) {
+      nodes {
+        name
+        childImageSharp {
+          gatsbyImageData(
+            layout: FIXED
+            placeholder: BLURRED
+            transformOptions: { grayscale: true }
+            width: 200
+            height: 200
+          )
+        }
+      }
+    }
+  }
+`
+const Gallery = () => {
+  const data = useStaticQuery(query)
+  const nodes = data.allFile.nodes
+  return (
+    <SWrapper>
+      {nodes.map((image, index) => {
+        const { name } = image
+        return (
+          <article key={index} className="item">
+            <GatsbyImage 
+            image={image.childImageSharp.gatsbyImageData}
+            alt={name}
+            className="gallery-img"
+            />
+            <p>{name}</p>
+          </article>
+        )
+      })}
+    </SWrapper>
+  )
+}
+
+const SWrapper = styled.section`
+ display: flex;
+ flex-wrap: wrap;
+ .item {
+   margin-right: 1rem;
+ }
+ .gallery-img {
+   border-radius: 1rem;
+ }
+`
+
+export default Gallery
