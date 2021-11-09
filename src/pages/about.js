98da988b1394a917d0c24e7e24ae941e446e643d
kenbom <tsutomu.kimura@nifty.ne.jp>
2021-11-09 05:23:53 +0000

    naza

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,15 +1,15 @@
-import React from 'react'
+import React from "react"
 import Layout from "../components/Layout"
-import {StaticImage} from "gatsby-plugin-image"
-import {Link} from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
+import { Link } from "gatsby"
 
 const About = () => {
-    return (
-      <Layout>
-        <main classNamae="page">
-          <section className="about-page">
-            <article>
-              <h2>I'm baby coloring book poke taxidermy</h2>
+  return (
+    <Layout>
+      <main classNamae="page">
+        <section className="about-page">
+          <article>
+            <h2>I'm baby coloring book poke taxidermy</h2>
             <p>
               Taxidermy forage glossier letterpress heirloom before they sold
               out you probably haven't heard of them banh mi biodiesel chia
@@ -18,18 +18,21 @@ const About = () => {
               Taiyaki tumblr flexitarian jean shorts brunch, aesthetic salvia
               retro.
             </p>
-          <Link to="/contact" className="btn" > contact </Link>
-</article>         
-        <StaticImage 
-        src="../assets/images/about.jpeg" 
-        alt="Person Pouring"
-        className="about-img"
-        placeholder="blurred"
-        />
-          </section>
-        </main>
-      </Layout>
-    )
+            <Link to="/contact" className="btn">
+              {" "}
+              contact{" "}
+            </Link>
+          </article>
+          <StaticImage
+            src="../assets/images/about.jpeg"
+            alt="Person Pouring"
+            className="about-img"
+            placeholder="blurred"
+          />
+        </section>
+      </main>
+    </Layout>
+  )
 }
 
 export default About
